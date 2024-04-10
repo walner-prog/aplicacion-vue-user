0932a65f82c7fe3be89591575e1ba195766562c4
walner <git@walner.cl>
2024-04-09 22:15:45 -0600

    Mensaje descriptivo de los camb2

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
     <div class="container-fluid">
-      <a class="navbar-brand active" href="#">Productos</a>
+      <a class="navbar-brand active" href="#">Usuarios</a>
       <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
       </button>
